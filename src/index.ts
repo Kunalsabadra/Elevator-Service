c09f062e623dc1e627104f29546fc30b42f2abec
kunalsabadra <kunalsabadra@gmail.com>
2024-05-01 15:54:45 +0530

    basic api setup done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 
 import serverConfig from './config/server.config';
+import apiRouter from './routes';
 
 const app: Express = express();
+
+app.use('/api', apiRouter);
+
+// app.get('/api/test', (req: Request, res: Response) => {
+//     res.json({ msg: "Hello" })
+// })
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server Started at PORT ${serverConfig.PORT}`);
---
+++ src/routes/v1/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { pingCheck } from '../../controllers/pingController';
+
+const v1Router = express.Router();
+
+v1Router.get('/ping', pingCheck);
+
+
+
+export default v1Router;
